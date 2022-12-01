0d91e35cc5838da28e3ccab417194439b238a17a
Rolen T. Le <rolentle@gmail.com>
2022-12-01 17:49:37 -0500

    refactor and rename

--- lib/advent_2022.rb
+++ lib/advent_2022.rb
@@ -10,24 +10,26 @@ module Advent2022
     prop :calories, T::Array[Integer]
   end
 
-  sig { params(list_of_calories: String).returns(Elf) }
-  def self.elf_with_most_calories(list_of_calories)
-    elf_store = elf_calories_store(list_of_calories)
-    T.must(elves_order_by_calorie_count(elf_store).last)
+  Elves = T.type_alias { T::Array[Elf] }
+
+  sig { params(elves: Elves).returns(Elf) }
+  def self.elf_with_most_calories(elves)
+    T.must(elves_order_by_calorie_count(elves).last)
   end
 
-  sig { params(elf_store: T::Array[Elf]).returns(T::Array[Elf]) }
-  def self.elves_order_by_calorie_count(elf_store)
-    elf_store.sort do |a_elf, b_elf|
+  sig { params(elves: Elves).returns(Elves) }
+  def self.elves_order_by_calorie_count(elves)
+    elves.sort do |a_elf, b_elf|
       a_elf.calories.sum <=> b_elf.calories.sum
     end
   end
 
-  sig { params(list_of_calories: String).returns(T::Array[Elf]) }
+  sig { params(list_of_calories: String).returns(Elves) }
   def self.elf_calories_store(list_of_calories)
     parsed_input = list_of_calories.split("\n\n").map do |individual_elf_calories|
       individual_elf_calories.split("\n").map(&:to_i)
     end
+
     parsed_input.map.with_index do |calories, index|
       Elf.new(id: index + 1, calories: calories)
     end
--- spec/day_1_spec.rb
+++ spec/day_1_spec.rb
@@ -23,7 +23,8 @@ describe 'day 1' do
 
   describe '.elf_with_most_calories' do
     it 'returns the 4th elf' do
-      expect(Advent2022.elf_with_most_calories(example_input).id).to eq(4)
+      elves = Advent2022.elf_calories_store(example_input)
+      expect(Advent2022.elf_with_most_calories(elves).id).to eq(4)
     end
   end
 
@@ -44,20 +45,21 @@ describe 'day 1' do
   describe 'for day_1_input' do
     it 'returns the 209th elf' do
       fixture_input = File.read('./spec/fixtures/day_1_input.txt')
-      expect(Advent2022.elf_with_most_calories(fixture_input).id).to eq(209)
+      elves = Advent2022.elf_calories_store(fixture_input)
+      expect(Advent2022.elf_with_most_calories(elves).id).to eq(209)
     end
 
     it 'returns the 74_198 total_calories for the 209th elf' do
       fixture_input = File.read('./spec/fixtures/day_1_input.txt')
-      store = Advent2022.elf_calories_store(fixture_input)
-      elf = store.find { |e| e.id == 209 }
+      elves = Advent2022.elf_calories_store(fixture_input)
+      elf = elves.find { |e| e.id == 209 }
       expect(elf.calories.sum).to eq(74_198)
     end
 
     it 'returns top 3 elves by calorie count' do
       fixture_input = File.read('./spec/fixtures/day_1_input.txt')
-      store = Advent2022.elf_calories_store(fixture_input)
-      elves_order_by_calorie_count = Advent2022.elves_order_by_calorie_count(store)
+      elves = Advent2022.elf_calories_store(fixture_input)
+      elves_order_by_calorie_count = Advent2022.elves_order_by_calorie_count(elves)
       top_three_elves = elves_order_by_calorie_count.last(3)
       total_calories = top_three_elves.map(&:calories).map(&:sum).sum
       expect(total_calories).to eq(209_914)
